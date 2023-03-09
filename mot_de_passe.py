1a21805c1d3dd31d4fec6465faf9ced424c6e482
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-09 21:12:06 +0100

    Ajout des ###### avant et après

--- mot_de_passe.py
+++ mot_de_passe.py
@@ -8,5 +8,6 @@ while not mot_de_passe == "superman":
     mot_de_passe = input("Quel est votre mot de passe ?")
     print("Le mot de passe renseigné est incorect !")
 
-
+print("#######################")
 print(" ***** Mot de passe accepté, Bienvenue !!! :) *****")
+print("#######################")
