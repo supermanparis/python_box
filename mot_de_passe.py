dc891e431f3ecab6f474f563def5cb7fe4053bba
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-09 21:02:12 +0100

    fichier test mot de pass

---
+++ mot_de_passe.py
@@ -0,0 +1,9 @@
+mot_de_passe = ""
+
+while not mot_de_passe == "yo":
+    
+    mot_de_passe = input("Quel est votre mot de passe ?")
+    print("Le mot de passe renseigné est incorect !")
+
+
+print(" ***** Mot de passe accepté, Bienvenue !!! :) *****")
