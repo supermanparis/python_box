aa10b33d7ec4730f76130e10810e65d42f640862
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-10 18:04:42 +0100

    Premier programme V2

--- premier_programme.py
+++ premier_programme.py
@@ -1,17 +1,23 @@
-#on distingue 3 Parties :
+# definition de la fonction 
+def demander_age():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("Quel est votre age ? ")
+        try:
+            age_int = int(age_str)
+        except ValueError:
+            print("ERREUR: Vous devez rentrer un nombre pour l'age")
+    return age_int
+    
 
 # Partie 1 : Demander nom
 nom = ""
 while nom == "":
     nom = input("Quel est votre nom ? ")
-# Partie 2 : Demander age
-age = 0
-while age == 0:
-    age_str = input("Quel est votre age ? ")
-    try:
-        age = int(age_str)
-    except ValueError:
-        print("ERREUR: Vous devez rentrer un nombre pour l'age")
+
+# Partie 2 : Demander age avec l'APPEL dela fonction et la definition de la variable age
+age = demander_age()
+
 # Partie 3 : Afficher les infos
 print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
 print("L'an prochain vous aurez " + str(age+1) + " ans")
