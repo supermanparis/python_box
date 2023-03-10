96b10d1be46b34c7d8b8505e0b29f11ac24eaf1b
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-10 11:12:32 +0100

    Ajout de la comptabilisation du nombre de tentatives

--- mot_de_passe2.py
+++ mot_de_passe2.py
@@ -1,6 +1,11 @@
+#comptabilisation du nombre de tentatives
+
 mot_de_pass = ""
+tentative = 0
 
 while not mot_de_pass == "mksdcndcqklsn":
     mot_de_pass = input("Quel est votre mot de passe? ")
+    tentative = tentative + 1
+    print("Tentative N°: "+ str(tentative))
 
-print("Accès Autorisé, Bienvenue !")
+print("Accès Autorisé, Bienvenue !, vous avez réussit au bout de la tenative numéro " + str(tentative))
