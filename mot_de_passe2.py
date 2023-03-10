48c9eaf0ca69811c8be9719bd33818106a500c50
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-10 12:26:10 +0100

    Ajout de if et else

--- mot_de_passe2.py
+++ mot_de_passe2.py
@@ -1,4 +1,4 @@
-#comptabilisation du nombre de tentatives
+#comptabilisation du nombre de tentatives + ajout de if et else
 
 mot_de_pass = ""
 tentative = 0
@@ -6,6 +6,10 @@ tentative = 0
 while not mot_de_pass == "mksdcndcqklsn":
     mot_de_pass = input("Quel est votre mot de passe? ")
     tentative = tentative + 1
-    print("Tentative N°: "+ str(tentative))
 
-print("Accès Autorisé, Bienvenue !, vous avez réussit au bout de la tenative numéro " + str(tentative))
+    if not mot_de_pass == "mksdcndcqklsn":
+        print("La tentative N°: "+ str(tentative) + " est incorrect!")
+    else :
+        print("Accès Autorisé, Bienvenue !, vous avez réussit au bout de la tenative numéro " + str(tentative))
+
+
