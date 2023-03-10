f1bef6f171bf91e85f2a705329da844109bff36b
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-10 17:44:17 +0100

    Mise en exergue des 3 parties du programme

--- premier_programme.py
+++ premier_programme.py
@@ -1,11 +1,10 @@
-"""Premier programme
-Formation Python
-apprendre la programmation"""
+#on distingue 3 Parties :
 
+# Partie 1 : Demander nom
 nom = ""
 while nom == "":
     nom = input("Quel est votre nom ? ")
-
+# Partie 2 : Demander age
 age = 0
 while age == 0:
     age_str = input("Quel est votre age ? ")
@@ -13,7 +12,7 @@ while age == 0:
         age = int(age_str)
     except ValueError:
         print("ERREUR: Vous devez rentrer un nombre pour l'age")
-
+# Partie 3 : Afficher les infos
 print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
 print("L'an prochain vous aurez " + str(age+1) + " ans")
 
