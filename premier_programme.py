2ac3f6c850341b8ed7cfc412d306085328f09d4e
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-11 14:11:31 +0100

    Ajout nom1 et nom2 + age1 et age2

--- premier_programme.py
+++ premier_programme.py
@@ -3,22 +3,25 @@
 def demander_nom():
     reponse_nom = ""
     while reponse_nom =="":
-        reponse_nom = input("Quel est votre nom? ")
+        reponse_nom = input("quel est votre nom? ")
     return reponse_nom
     
 
 
 # definition de la fonction 
-def demander_age():
+def demander_age(nom_personne):
     age_int = 0
     while age_int == 0:
-        age_str = input("Quel est votre age ? ")
+        age_str = input(nom_personne + " ,Quel est votre age ? ")
         try:
             age_int = int(age_str)
         except ValueError:
             print("ERREUR: Vous devez rentrer un nombre pour l'age")
     return age_int
     
+
+
+
 '''
 # Partie 1 : Demander nom
 nom = ""
@@ -27,12 +30,16 @@ while nom == "":
 '''
 
 # Partie 2 : Demander age + demander nom  avec l'APPEL des foncions demander_age() et demander_nom() et la definition des variables
-age = demander_age()
-nom = demander_nom()
+nom1 = demander_nom()
+nom2 = demander_nom()
+age1 = demander_age(nom1)
+age2 = demander_age(nom2)
+
 
 # Partie 3 : Afficher les infos
-print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
-print("L'an prochain vous aurez " + str(age+1) + " ans")
+print("Vous vous appelez " + nom1 + ", vous avez " + str(age1) + " ans")
+print("L'an prochain vous aurez " + str(age1+1) + " ans")
 
-
+print("Vous vous appelez " + nom2 + ", vous avez " + str(age2) + " ans")
+print("L'an prochain vous aurez " + str(age2+1) + " ans")
 
