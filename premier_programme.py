94d2670e7d0e14498eefde63f8784d69479d5963
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-11 13:14:41 +0100

    Ajout de la definition de la fonction demander_nom()

--- premier_programme.py
+++ premier_programme.py
@@ -1,3 +1,13 @@
+# definition de la fonction demander nom
+
+def demander_nom():
+    reponse_nom = ""
+    while reponse_nom =="":
+        reponse_nom = input("Quel est votre nom? ")
+    return reponse_nom
+    
+
+
 # definition de la fonction 
 def demander_age():
     age_int = 0
@@ -9,14 +19,16 @@ def demander_age():
             print("ERREUR: Vous devez rentrer un nombre pour l'age")
     return age_int
     
-
+'''
 # Partie 1 : Demander nom
 nom = ""
 while nom == "":
     nom = input("Quel est votre nom ? ")
+'''
 
-# Partie 2 : Demander age avec l'APPEL dela fonction et la definition de la variable age
+# Partie 2 : Demander age + demander nom  avec l'APPEL des foncions demander_age() et demander_nom() et la definition des variables
 age = demander_age()
+nom = demander_nom()
 
 # Partie 3 : Afficher les infos
 print("Vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
