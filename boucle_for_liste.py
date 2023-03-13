4fbd9f6d936f31ac12ed4f58dd48a61ec66f915e
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-13 12:45:33 +0100

    Fichier boucle for avec une liste

---
+++ boucle_for_liste.py
@@ -0,0 +1,6 @@
+ma_liste = ["VA","YE","LE","HA","AB","AM","SA","AB","NA","FA","YA", 
+"SA", "GH", "AN", "IL", "TA", "LA", "BA", "IB", "AM"]
+
+for i in ma_liste:
+    print(20*(i + " is happy!!!" ))
+
