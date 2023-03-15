c867ecc56ce34165f8caca86f3c556d4f0a61211
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-15 14:01:20 +0100

    boucle for liste ok

--- boucle_for_liste.py
+++ boucle_for_liste.py
@@ -1,5 +1,5 @@
 ma_liste = ["VA","YE","DA","NA","AB","AM","SA","HA","NA","FA"]
 
 for i in ma_liste:
-    print(*(i + " is happy! :)"))
+    print(10*(i + " is happy! :)"))
     print(5*("Oui oui "+ i +" is happy! :)" ))
