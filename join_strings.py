7fdf745c9ae88d678fa088c6d7afd0508a4d6798
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-19 14:27:08 +0100

    join_strings

---
+++ join_strings.py
@@ -0,0 +1,8 @@
+# join strings
+# joindre les elements d'une liste en une string
+
+
+test = ['I', 'Like', 'Python', 'automation']
+print(' '.join(test)) # joindre les éléménents de la liste test avec 1 espace entre chaque élément
+
+print('***'.join(test)) # joindre les éléménents de la liste test avec 3 étoiles entre chaque élément
