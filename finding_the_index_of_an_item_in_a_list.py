e595753511898703252b7a2fd01e1cb80ab29b51
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-19 14:16:59 +0100

    finding_the_index_of_an_item_in_a_list

---
+++ finding_the_index_of_an_item_in_a_list.py
@@ -0,0 +1,8 @@
+#  Finding the index of an item in a list
+
+list_a = ['foo','bar','baz'] # création de la variable list_a ayant 3 elements dans sa liste
+list_a.index('bar') # savoir quel est l'index de l'item "bar" dans la list_a ?
+
+print(list_a.index('bar')) # ici bar est situé à l'index 1 ( donc en deuxieme position)
+
+print(list_a.index('baz')) # ici bar est situé à l'index 1 ( donc en troisième position)
