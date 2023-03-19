892058b055e92fef8b1eb6c8941c6f2663ce00cd
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-19 14:20:30 +0100

    inverser une string ou une liste

---
+++ reversing_strings_and_lists_3.py
@@ -0,0 +1,10 @@
+# Reversing strings and lists 3
+# inverser une string ou une liste
+
+print('Test Python'[::-1])
+
+print('mlekfvmnfmrjnvmlsfkdvlskfvk'[::-1])
+
+liste = ["a","b","c",1,2,3]
+
+print(liste[::-1])
