892aed51f143c8f53604b13e1529ffef164e4893
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-19 13:26:23 +0100

    chaining_of_comparison_operators

---
+++ chaining_of_comparison_operators.py
@@ -0,0 +1,12 @@
+# Chaining Of Comparison Operators
+
+
+n = 10
+result = 1 < n < 20  # ici est-ce que le nombre est supérieur à 1, ET inférieur à 20?
+print(result) # ici la reponse sera exprimée en booléen True or False
+
+
+
+
+result = 1 > n <= 9 # ici on demande n est égal à 1 ou inférieur ou égal à 9?
+print(result) # ici la reponse sera exprimée en booléen True or False
