9246845d48723baa80cc3741756ba75ab4126e50
spaceadventure <51682334+supermanparis@users.noreply.github.com>
2023-03-19 14:29:25 +0100

    ajout d'une méthode avec .reverse()

--- reversing_strings_and_lists_3.py
+++ reversing_strings_and_lists_3.py
@@ -8,3 +8,9 @@ print('mlekfvmnfmrjnvmlsfkdvlskfvk'[::-1])
 liste = ["a","b","c",1,2,3]
 
 print(liste[::-1])
+
+# Reversing strings and lists autre méthode :
+
+testList = [1, 3, 5]
+testList.reverse()
+print(testList)
